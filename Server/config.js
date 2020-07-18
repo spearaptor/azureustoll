080295655cb495eed1ac30c6fef656dd2bafe727
Somya Didwania <35562354+somyadidwania@users.noreply.github.com>
2020-07-18 23:33:21 +0530

    Server endpoints for create user, check balance, revoke user is created

---
+++ Server/config.js
@@ -0,0 +1,10 @@
+// CONFIG***
+module.exports = {
+    apiPrefix: 'https://beta-api.ethvigil.com/v0.1/contract/',
+    apiKey: 'bd31edb1-ceed-4532-840d-c69d9879e9a6',
+    contractAddress: '0x269e983987ad5e3123dae44d5561c28575ed7ea4',
+    //TransferBot: '0xd11430bbadc6f921da97dc6e9843a5a56868a144',
+    auditContractAddress: '0xf2de5bf15565f3170225d6613a3e547f531f7207',
+    erc20minContractAddress: '0xa76d4cc5e29d39f64a0e876dfdf1d59b5f209b09',
+    wsUrl: 'wss://beta.ethvigil.com/ws'
+}
