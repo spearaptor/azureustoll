67fe82fe11ba5bb705e16865be858980937eec36
Somya Didwania <35562354+somyadidwania@users.noreply.github.com>
2020-07-28 00:10:31 +0530

    function call to remove data from csv when revoke endpoint is called

--- Server/api/controllers/tollController.js
+++ Server/api/controllers/tollController.js
@@ -1,7 +1,19 @@
 const request = require('request');
 const axios = require('axios');
 const config = require('./../../config.js');
-const {createEthaddress, tollInfoHashes, userInfoHashes, createHashes, generateTollQR, getPayableAmount} = require('./../../services/services');
+const {
+    createEthaddress,
+    tollInfoHashes,
+    userInfoHashes,
+    createHashes,
+    generateTollQR,
+    getPayableAmount,
+    createUpdateCSV,
+    removeRowInCSV
+    } = require('./../../services/services');
+
+const userFields = ['Email', 'Ethaddress', 'LicenceId', 'AccountVerified'];
+const tollFields = ['Email', 'Ethaddress', 'DocumentId', 'Pricing', 'AccountApproved'];
 
 const tokenInstance = axios.create({
 	baseURL: config.apiPrefix + config.contractAddress,
@@ -23,9 +35,11 @@ const erc20Mintable = axios.create({
 
 // Create User Account By Email
 exports.create_user = function(req, res) {
-    const userEthaddress = createEthaddress(req.body.emailAddress);
-    const userEmailHash = createHashes(req.body.emailAddress);
-    const userInfoHash = userInfoHashes(req.body.licence);
+    const email = req.body.emailAddress;
+    const licenceId = req.body.licence;
+    const userEthaddress = createEthaddress(email);
+    const userEmailHash = createHashes(email);
+    const userInfoHash = userInfoHashes(licenceId);
     // console.log("POST REQUEST", req.body.emailAddress, req.body.licence, req.body);
     tokenInstance.post('/addUserByEmail', {
         ethAddress: userEthaddress,
@@ -36,6 +50,14 @@ exports.create_user = function(req, res) {
         const data = response.data;
         data["ethAddress"] = userEthaddress;
         console.log("RESPONSE FROM API", data);
+        let userFileData = {
+            'Email' : email,
+            'Ethaddress' : userEthaddress,
+            'LicenceId' : licenceId,
+            'AccountVerified' : '1'
+        }//1 for approved and 0 for yet to approve and -1 for rejected.
+        createUpdateCSV(userFields, userFileData, "userData");
+        console.log( "UserCSV:",userFileData);
         res.send(data);
     })
     .catch(function (error) {
@@ -61,7 +83,15 @@ exports.create_toll = function(req, res) {
         .then(function (response) {
             const data = response.data;
             data["ethAddress"] = tollEthaddress;
-            console.log("RESPONSE FROM API", data);
+            let tollFileData = {
+                'Email'         : req.body.emailAddress,
+                'Ethaddress'    : tollEthaddress,
+                'DocumentId'    : req.body.documentId,
+                'Pricing'       : req.body.tollPricing,
+                'AccountApproved':'1',
+            }//1 for approved and 0 for yet to approve and -1 for rejected.
+            createUpdateCSV(tollFields, tollFileData, "TollData")
+            console.log("RESPONSE FROM API", data, tollFileData);
             res.type('svg');
             qr.pipe(res);
         })
@@ -83,6 +113,7 @@ exports.revoke_user = function(req, res) {
     .then(function (response) {
         const data = response.data;
         console.log(data);
+        removeRowInCSV(userEthaddress, "userData");
         // if (data.success){
         //     console.log('User '+ ethaddress+' By Email added');
         // }
@@ -105,6 +136,7 @@ exports.revoke_toll = function(req, res) {
     .then(function (response) {
         const data = response.data;
         console.log(data);
+        removeRowInCSV(tollEthaddress, "TollData");
         // if (data.success){
         //     console.log('User '+ ethaddress+' By Email added');
         // }
