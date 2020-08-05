d432251fe3c255c214b0a5185343c6be35de9c61
shubidiwoop <shubhamsharma1698@gmail.com>
2020-08-05 23:03:27 +0530

    space-daemon

---
+++ api/routes/tollRoutes.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = function(app) {
+  var tollUsers = require('../controllers/tollController');
+
+  // TOLL Tax Contract Routes
+  app.route('/toll/createUser')
+    .post(tollUsers.create_user);
+  
+  app.route('/toll/createToll')
+    .post(tollUsers.create_toll);
+
+  app.route('/toll/revokeUser')
+    .post(tollUsers.revoke_user);
+  
+  app.route('/toll/mint')
+    .post(tollUsers.mint);
+  
+  app.route('/toll/revokeToll')
+    .post(tollUsers.revoke_toll);
+  
+  app.route('/toll/payToll')
+    .post(tollUsers.pay_toll);
+
+  // ERC20Mintable contract Routes
+  app.route('/erc20/balanceOf')
+    .post(tollUsers.balanceOf);
+
+};
---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    apiPrefix: 'https://beta-api.ethvigil.com/v0.1/contract/',
+    apiKey: process.env.ETHAPIKEY,
+    contractAddress: '0xaf16ec1cf012a9fd8d507ae683876afc5effa370',
+    //TransferBot: '0xd11430bbadc6f921da97dc6e9843a5a56868a144',
+    auditContractAddress: '0xf2de5bf15565f3170225d6613a3e547f531f7207',
+    erc20minContractAddress: '0x06e39284325c3c82f4d51b79ee585f5bfaaf85b4',
+    wsUrl: 'wss://beta.ethvigil.com/ws'
+}
